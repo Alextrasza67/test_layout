999ea74681f4f1e29beb6270ced82dae358b1932
Alextrasza67 <alextrasza67@163.com>
2020-04-09 14:37:22 +0800

    #commit 横向连续跑马灯 修改为数据可以变动

--- src/view/test/col_scroll.vue
+++ src/view/test/col_scroll.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="col-scroll-div" id="PackagingShell">
     <div class="scroll-container" id="viewBox">
-      <span class="scroll-text" >{{text}}</span>
-      <span class="scroll-transcript" id="transcript"></span>
+      <span class="scroll-text">{{text}}</span>
+      <span class="scroll-transcript" id="transcript1"></span>
+      <span class="scroll-transcript" id="transcript2"></span>
     </div>
     <div class="hide-container" id="hide">
-      <span class="scroll-text" >{{text}}</span>
+      <span class="scroll-text">{{text}}</span>
     </div>
   </div>
 </template>
@@ -15,16 +16,39 @@
     name: "col_scroll",
     data() {
       return {
-        text: '这是一个测试跑马灯这是一个测试跑马灯这是一个测试跑马灯'
+        text: '这是一个测试跑马灯这是一个测试跑马灯这是一个测试跑马灯这是一个测试跑马灯这是一个测试跑马灯',
+        space: 50,
+        hasRefresh: false,
+        newText: '',
+        interval: null,
+        // 设置初始位移距离
+        distance: 0
       }
     },
     created() {
     },
     mounted() {
       this.move();
+      this.refreshDate();
     },
     methods: {
-      move(){
+      refreshDate(){
+        let _this = this
+        setInterval(function () {
+          if(!_this.hasRefresh){
+            _this.newText = (Math.random() * 100).toFixed(0)+'这是一个测试跑马灯这是一个测试跑马灯这是一个测试跑马灯这是一个测试跑马灯这是一个测试跑马灯'
+          }
+        }, 3000);
+      },
+      resetTranscript(id, text) {
+        // 内容超长,则获取抄本元素
+        var transcript = document.getElementById(id)
+        // 抄本元素内容填充
+        transcript.innerText = text
+        // 设定抄本和主题之间的间距
+        transcript.style.marginLeft = this.space+'px'
+      },
+      move() {
         // 首先获取整个内容的宽度,(展示内容会超出隐藏,无法通过获取盒子宽度取到实际的内容宽度)
         // 所以专门造了一个 div 块来放内容,隐藏起来 (无法用根据字体内容来计算宽度的方法,存在误差,当内容较多
         // 的时候误差会相对较大)
@@ -35,31 +59,35 @@
         let viewBox = document.getElementById('viewBox')
         // 判断内容是否超长
         if ((BoxWidth - width) < 0) {
-          // 内容超长,则获取抄本元素
-          let transcript = document.getElementById('transcript')
-          // 抄本元素内容填充
-          transcript.innerText = this.text
-          // 设定抄本和主题之间的间距
-          transcript.style.marginLeft = '50px'
+          this.resetTranscript('transcript1', this.text);
+          this.resetTranscript('transcript2', this.text);
         } else {
           // 没有超长则return,不做任何操作
           return
         }
-        // 设置初始位移距离
-        let distance = 0
         // 移动函数,通过定时器实现
-        setInterval(function () {
+        let _this = this
+        this.interval = setInterval(function () {
           // 位移内用记录是是递减,此处的 1 控制移动变量 s
-          distance = distance - 1
+          _this.distance = _this.distance - 1
           // 判断是否整个内容移动完
-          if (-distance >= width) {
+          if (-_this.distance >= (2 * width + _this.space)) {
             // 若移动完,则重新设定位移值,此处赋值200是正好把 上面设置的块间距  200px 也加入移动范围,实现移动完无缝跳转
-            distance = 50
+            _this.distance = _this.space
+            _this.text = _this.newText
+            _this.hasRefresh = false
+            clearInterval(_this.interval)
+            _this.move()
+          }
+          if(!_this.hasRefresh && -_this.distance>=width){
+            _this.hasRefresh = true
+            _this.resetTranscript('transcript2', _this.newText);
           }
           // 实时设置位移距离
-          viewBox.style.transform = 'translateX(' + distance + 'px)'
+          viewBox.style.transform = 'translateX(' + _this.distance + 'px)'
         }, 27) // 移动时间间隔t   s和t 共同决定移动速度
-      }
+      },
+
     }
   }
 </script>
@@ -67,7 +95,7 @@
 <style lang="scss" scoped>
   .col-scroll-div {
     width: 50%;
-    margin: 0 auto;
+    margin: 25% auto;
     overflow: hidden;
 
     .scroll-container {
@@ -78,7 +106,7 @@
       white-space: nowrap;
     }
 
-    .hide-container{
+    .hide-container {
       width: 0px;
       overflow: hidden;
       font-size: 24px;
